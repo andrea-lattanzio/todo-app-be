fd813a48525bd59054d0b4955a440cc4fdb4199d
Ltensio <76816528+Ltensio@users.noreply.github.com>
2025-01-26 11:08:42 +0100

    feat: update main

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import helmet from 'helmet';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.setGlobalPrefix('/api');
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true
+    }),
+  );
+
+  app.use(helmet({ contentSecurityPolicy: false }));
+  app.enableCors();
+
+  const configService: ConfigService = app.get<ConfigService>(ConfigService);
+  const port: number = configService.getOrThrow<number>('PORT');
+  await app.listen(port);
+  console.log(`🚀 Application is running on: http://localhost:${port}/api`);
 }
 bootstrap();
