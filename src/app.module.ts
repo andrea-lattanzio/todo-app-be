3e4032bfd9cbe1b00ff647cc08833deba311ee17
Ltensio <76816528+Ltensio@users.noreply.github.com>
2025-01-26 11:09:21 +0100

    feat: create todo api

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DatabaseModule } from './config/database/database.module';
+import { ConfigModule } from '@nestjs/config';
+import { TodoModule } from './todo/todo.module';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot({isGlobal: true}), DatabaseModule, TodoModule],
   controllers: [AppController],
   providers: [AppService],
 })
