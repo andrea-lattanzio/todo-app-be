20983d503b40af4c22f16aaec80aa4d2daacd8dc
Ltensio <76816528+Ltensio@users.noreply.github.com>
2025-01-28 18:02:27 +0100

    feat: add files for category crud operations

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,8 +4,9 @@ import { AppService } from './app.service';
 import { DatabaseModule } from './config/database/database.module';
 import { ConfigModule } from '@nestjs/config';
 import { TaskModule } from './task/task.module';
+import { CategoryModule } from './category/category.module';
 @Module({
-  imports: [ConfigModule.forRoot({isGlobal: true}), DatabaseModule, TaskModule],
+  imports: [ConfigModule.forRoot({isGlobal: true}), DatabaseModule, TaskModule, CategoryModule],
   controllers: [AppController],
   providers: [AppService],
 })
