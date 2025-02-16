1c2f6dc0915abb4be405e3c5539136ee335d4b5f
Ltensio <76816528+Ltensio@users.noreply.github.com>
2025-02-16 14:48:01 +0100

    update scripts and config module for new env files

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,16 +2,17 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DatabaseModule } from './config/database/database.module';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TaskModule } from './task/task.module';
 import { CategoryModule } from './category/category.module';
 import { AuthModule } from './identity/auth/auth.module';
 import { UserModule } from './identity/user/user.module';
 import { JwtGuard } from './common/guards/jwt.guard';
 import { APP_GUARD } from '@nestjs/core';
+import { join } from 'path';
 @Module({
   imports: [
-    ConfigModule.forRoot({ isGlobal: true }),
+    ConfigModule.forRoot({ isGlobal: true, envFilePath: getEnvPath() }),
     DatabaseModule,
     TaskModule,
     CategoryModule,
@@ -22,3 +23,9 @@ import { APP_GUARD } from '@nestjs/core';
   providers: [AppService, { provide: APP_GUARD, useClass: JwtGuard}],
 })
 export class AppModule {}
+
+function getEnvPath(): string {
+  const configService = new ConfigService();
+  const env: string = configService.get<string>("NODE_ENV") || "development";
+  return join(`.env.${env}`);
+}
